b7b1ea38340bc1ca1e9c66fbd482e24e6e684bde
mellesacha <123104912+mellesacha@users.noreply.github.com>
2023-08-11 23:43:06 +0400

    2

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,84 +1,140 @@
-import { Component } from "react";
+import { Component, useState, useEffect } from "react";
 import { nanoid } from 'nanoid';
 import ContactForm from "./ContactForm";
 import ContactList from "./ContactList";
 import Filter from "./Filter";
 import { Container, Title } from "./App.styled";
 
-export class App extends Component {
+export const App = () => {
 
-  state = {
-   contacts: [
-    {id: 'id-1', name: 'Rosie Simpson', number: '459-12-56'},
-    {id: 'id-2', name: 'Hermione Kline', number: '443-89-12'},
-    {id: 'id-3', name: 'Eden Clements', number: '645-17-79'},
-    {id: 'id-4', name: 'Annie Copeland', number: '227-91-26'},
-  ],
-  filter: '',
-  }
+  const [contacts, setContacts] = useState([
+    { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+    { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+    { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+    { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+  ]);
+  const [filter, setFilter] = useState('');
 
-  componentDidMount() {
+  useEffect(() => {
 
-    const listContact = localStorage.getItem("Contacts");
-    const parseListContact = JSON.parse(listContact);
-    localStorage.length && this.setState({contacts : parseListContact})
-    
-  }
+    const listContact = JSON.parse(localStorage.getItem("Contacts"));
+    localStorage.length && setContacts(listContact);
 
-  componentDidUpdate() {
-    const {contacts} = this.state;
+  }, []);
+
+  useEffect(() => {
     localStorage.setItem("Contacts", JSON.stringify(contacts));
-  }
+  }, [contacts]);
 
-  onAddContact = (contact) => {
-    const newContact = {
-      ...contact,
-      id: nanoid()
-    }
-
-    this.state.contacts.find(contact => contact.name.toLowerCase() === newContact.name.toLowerCase())
-          ? (alert(`${newContact.name} is already in contacts`))
-      : this.setState(prevState => { return { contacts: [{ ...newContact }, ...prevState.contacts] } }) 
+  const onAddContact = (contact) => {
   
-    // this.setState(prevState => 
-    //   this.state.contacts.find(contact => contact.name.toLowerCase() === newContact.name.toLowerCase())
-    //       ? (alert(`${newContact.name} is already in contacts`))
-    //       : { contacts: [{ ...newContact }, ...prevState.contacts] }
-    // )
-
+      const newContact = {
+        ...contact,
+        id: nanoid()
+      }
+ 
+    contacts.find(contact => contact.name.toLowerCase() === newContact.name.toLowerCase())
+      ? (alert(`${newContact.name} is already in contacts`))
+      : setContacts(contacts => { return [newContact, ...contacts] } )
   }
 
-  deleteContact = (id) => {
-    this.setState((prevState) => {
-      return {
-        contacts: prevState.contacts.filter((contact) => contact.id !== id),
-      };
+   const deleteContact = (id) => {
+    setContacts((contacts) => {
+      return  contacts.filter((contact) => contact.id !== id)
     });
   };
 
-  handlInputFilter = (e) => {
-    this.setState({ filter: e.target.value })
+  const handlInputFilter = (e) => {
+    setFilter(e.target.value )
   }
 
-
-  render()
-  {
-    const { contacts, filter } = this.state;
-    const filteredContacts = contacts.filter(contact =>
+  const filteredContacts = contacts.filter(contact =>
       contact.name.toLowerCase().includes(filter.toLowerCase().trim())
-    );
+    )
 
-    return (
+
+  return (
  <Container>
   <Title>Phonebook</Title>
-        <ContactForm addContact={this.onAddContact } />
+        <ContactForm addContact={onAddContact } />
 
   <Title>Contacts</Title>
-        <Filter search={filter}  handlInput={this.handlInputFilter}/>
-        <ContactList contacts={filteredContacts} onDeleteContact={ this.deleteContact} />
+        <Filter search={filter}  handlInput={handlInputFilter}/>
+        <ContactList contacts={filteredContacts} onDeleteContact={deleteContact} />
 </Container>
     )
   
    
-  }
-};
+}
+
+// export class App extends Component {
+
+//   state = {
+//    contacts: [
+//     {id: 'id-1', name: 'Rosie Simpson', number: '459-12-56'},
+//     {id: 'id-2', name: 'Hermione Kline', number: '443-89-12'},
+//     {id: 'id-3', name: 'Eden Clements', number: '645-17-79'},
+//     {id: 'id-4', name: 'Annie Copeland', number: '227-91-26'},
+//   ],
+//   filter: '',
+//   }
+
+//   componentDidMount() {
+
+//     const listContact = localStorage.getItem("Contacts");
+//     const parseListContact = JSON.parse(listContact);
+//     localStorage.length && this.setState({contacts : parseListContact})
+    
+//   }
+
+//   componentDidUpdate() {
+//     const {contacts} = this.state;
+//     localStorage.setItem("Contacts", JSON.stringify(contacts));
+//   }
+
+//   onAddContact = (contact) => {
+//     const newContact = {
+//       ...contact,
+//       id: nanoid()
+//     }
+
+//     this.state.contacts.find(contact => contact.name.toLowerCase() === newContact.name.toLowerCase())
+//           ? (alert(`${newContact.name} is already in contacts`))
+//       : this.setState(prevState => { return { contacts: [{ ...newContact }, ...prevState.contacts] } }) 
+
+//   }
+
+//   deleteContact = (id) => {
+//     this.setState((prevState) => {
+//       return {
+//         contacts: prevState.contacts.filter((contact) => contact.id !== id),
+//       };
+//     });
+//   };
+
+//   handlInputFilter = (e) => {
+//     this.setState({ filter: e.target.value })
+//   }
+
+
+//   render()
+//   {
+//     const { contacts, filter } = this.state;
+//     const filteredContacts = contacts.filter(contact =>
+//       contact.name.toLowerCase().includes(filter.toLowerCase().trim())
+//     );
+
+//     return (
+//  <Container>
+//   <Title>Phonebook</Title>
+//         <ContactForm addContact={this.onAddContact } />
+
+//   <Title>Contacts</Title>
+//         <Filter search={filter}  handlInput={this.handlInputFilter}/>
+//         <ContactList contacts={filteredContacts} onDeleteContact={ this.deleteContact} />
+// </Container>
+//     )
+  
+   
+//   }
+// };
--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -1,39 +1,41 @@
-import { Component } from "react";
+import { Component, useState } from "react";
 import PropTypes from 'prop-types';
 import { Form, Label, Input, Button } from "./ContactForm.styled";
 
-class ContactForm extends Component  {
+const ContactForm = ({addContact}) => {
+    const [name, setName] = useState('');
+    const [number, setNumber] = useState('');
 
-    state = {
-        name: '',
-        number: ''
-    }
+    const handleInput = (e) => {
+        const { name, value } = e.target;
+        switch (name) {
+            case "name":
+                setName(value);
+                break;
+            
+            case "number":
+                setNumber(value);
+                break;
+            
+            default: return;
+        }
+            
+    };
 
-
-    
-    handleInput = (e) => {
-
-        const {name, value} = e.target
-
-        this.setState({
-            [name]: value,
-        })
-    }
+    const handleSubmit = (e) => {
+        e.preventDefault()
         
-    handleSubmit = (e) => {
-    e.preventDefault()
+        addContact({ name, number });
         
-        this.props.addContact({ ...this.state });
-        
-        this.setState({ name: "", number: "" });
+        setName('');
+        setNumber('');
         
         e.target.reset();
-  }     
-   
-    render() {
-       return (
+    };
 
-    <Form name="contact_form" onSubmit={this.handleSubmit}>
+    return (
+
+        <Form name="contact_form" onSubmit={handleSubmit}>
             <Label>Name
                 <Input
                     type="text"
@@ -41,7 +43,7 @@ class ContactForm extends Component  {
                     pattern="^[a-zA-Zа-яА-ЯІіЇїҐґ' \-\u0400-\u04FF]+$"
                     title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
                     required
-                    onChange={this.handleInput}
+                    onChange={handleInput}
                 /></Label>
             <Label>Number
                 <Input
@@ -50,15 +52,74 @@ class ContactForm extends Component  {
                     pattern="^[+]?[0-9\\.\\-\\s]{1,15}$"
                     title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
                     required
-                    onChange={this.handleInput}
+                    onChange={handleInput}
                 />
             </Label>
             <Button type="submit">Add contact</Button>
         </Form>
       
     )
-   } 
 };
+
+
+
+// class ContactForm extends Component  {
+
+//     state = {
+//         name: '',
+//         number: ''
+//     }
+
+
+    
+//     handleInput = (e) => {
+
+//         const {name, value} = e.target
+
+//         this.setState({
+//             [name]: value,
+//         })
+//     }
+        
+//     handleSubmit = (e) => {
+//     e.preventDefault()
+        
+//         this.props.addContact({ ...this.state });
+        
+//         this.setState({ name: "", number: "" });
+        
+//         e.target.reset();
+//   }     
+   
+//     render() {
+//        return (
+
+//     <Form name="contact_form" onSubmit={this.handleSubmit}>
+//             <Label>Name
+//                 <Input
+//                     type="text"
+//                     name="name"
+//                     pattern="^[a-zA-Zа-яА-ЯІіЇїҐґ' \-\u0400-\u04FF]+$"
+//                     title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+//                     required
+//                     onChange={this.handleInput}
+//                 /></Label>
+//             <Label>Number
+//                 <Input
+//                     type="tel"
+//                     name="number"
+//                     pattern="^[+]?[0-9\\.\\-\\s]{1,15}$"
+//                     title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+//                     required
+//                     onChange={this.handleInput}
+//                 />
+//             </Label>
+//             <Button type="submit">Add contact</Button>
+//         </Form>
+      
+//     )
+//    } 
+// };
 
 export default ContactForm;
 
